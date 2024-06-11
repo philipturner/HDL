cdcd5d7abe39f04573036a21b8d16c802940cad8
Philip Turner <philipturner.AR@gmail.com>
2024-06-11 17:25:43 -0400

    Added more materials

--- Sources/HDL/Lattice/Material.swift
+++ Sources/HDL/Lattice/Material.swift
@@ -80,6 +80,16 @@ extension Float {
     let materialType = closure()
     var cubicSpacing: Float
     switch materialType {
+    case .checkerboard(.boron, .nitrogen),
+        .checkerboard(.nitrogen, .boron):
+      cubicSpacing = 0.3615
+    case .checkerboard(.boron, .phosphorus),
+        .checkerboard(.phosphorus, .boron):
+      cubicSpacing = 0.4538
+    case .checkerboard(.boron, .arsenic),
+        .checkerboard(.arsenic, .boron):
+      cubicSpacing = 0.4778
+      
     case .elemental(.carbon):
       cubicSpacing = 0.3567
     case .checkerboard(.carbon, .silicon),
@@ -88,24 +98,41 @@ extension Float {
     case .checkerboard(.carbon, .germanium),
         .checkerboard(.germanium, .carbon):
       cubicSpacing = 0.4523
+      
+    case .checkerboard(.nitrogen, .aluminum),
+        .checkerboard(.aluminum, .nitrogen):
+      cubicSpacing = 0.4358
     case .checkerboard(.nitrogen, .gallium),
         .checkerboard(.gallium, .nitrogen):
       cubicSpacing = 0.4498
+      
+    case .checkerboard(.aluminum, .phosphorus),
+        .checkerboard(.phosphorus, .aluminum):
+      cubicSpacing = 0.5464
+    case .checkerboard(.aluminum, .arsenic),
+        .checkerboard(.arsenic, .aluminum):
+      cubicSpacing = 0.5660
+      
     case .elemental(.silicon):
       cubicSpacing = 0.5431
+      
     case .checkerboard(.phosphorus, .gallium),
         .checkerboard(.gallium, .phosphorus):
       cubicSpacing = 0.5450
+      
     case .checkerboard(.gallium, .arsenic),
         .checkerboard(.arsenic, .gallium):
       cubicSpacing = 0.5653
+      
     case .elemental(.germanium):
       cubicSpacing = 0.5658
+      
     case .elemental(.gold):
       cubicSpacing = 0.4078
       guard constantType == .square else {
         fatalError("Hexagonal gold is unsupported.")
       }
+      
     default:
       fatalError("Unrecognized material type: \(materialType)")
     }

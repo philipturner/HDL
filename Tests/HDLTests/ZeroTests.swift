c9020a03bda88737b46268738469fc954f299e12
Philip Turner <philipturner.AR@gmail.com>
2025-07-09 09:10:28 -0400

    Save progress

---
+++ Tests/HDLTests/ZeroTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import HDL
+
+// Unit test how various APIs respond when they receive arrays of zero length.
+//
+// APIs:
+//  Lattice -> Bounds -> atoms
+//  Lattice -> Bounds -> Volume/Plane -> atoms
+//    test both finite and zero bounds, with a plane that changes the output in
+//    the finite case
+
+
+final class ZeroTests: XCTestCase {
+  
+}

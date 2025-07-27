25506878907275ce50a8538985e4c81752a516ee
Philip Turner <philipturner.AR@gmail.com>
2025-07-27 11:20:12 -0400

    This change triggers something strange in the compiler

--- Sources/HDL/Topology/OctreeSorter/OctreeSorter+DynamicThreaded.swift
+++ Sources/HDL/Topology/OctreeSorter/OctreeSorter+DynamicThreaded.swift
@@ -160,6 +160,7 @@ extension OctreeSorter {
       }
       let childLatencies = createChildLatencies()
       
+      @inline(__always)
       func createMaximumTaskCount() -> Float {
         if levelSize <= 1 {
           return 1
@@ -198,13 +199,13 @@ extension OctreeSorter {
         let actualTaskCount = (idealTaskCount > 1) ? 8 : 1
         
         var output = WorkSplitting()
-//        if idealTaskCount > 1 {
-        output.taskCount = idealTaskCount > 1 ? 8 : 1
-        output.assignments = idealTaskCount > 1 ? SIMD8(0, 1, 2, 3, 4, 5, 6, 7) : SIMD8.zero
-//        } else {
-//          output.taskCount = 1
-//          output.assignments = SIMD8(0, 0, 0, 0, 0, 0, 0, 0)
-//        }
+        if idealTaskCount > 1 {
+          output.taskCount = 8
+          output.assignments = SIMD8(0, 1, 2, 3, 4, 5, 6, 7)
+        } else {
+          output.taskCount = 1
+          output.assignments = SIMD8(0, 0, 0, 0, 0, 0, 0, 0)
+        }
         return output
       }
       let workSplitting = createWorkSplitting()

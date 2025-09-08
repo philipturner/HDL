290f24a8820798f8248ba387ac56e86db852797a
Philip Turner <philipturner.AR@gmail.com>
2025-09-08 12:47:21 -0400

    Starting the organize and work on the function body

--- Sources/HDL/Topology/OctreeSorter/OctreeSorter+HighLevels.swift
+++ Sources/HDL/Topology/OctreeSorter/OctreeSorter+HighLevels.swift
@@ -5,92 +5,101 @@
 //  Created by Philip Turner on 9/7/25.
 //
 
+import Dispatch
+
+// Pseudocode of traversal algorithm:
+//
+// Each parent thread gets allocated n * 8 child threads, where n is the cell
+// count in the parent thread. Each possible child up to 8 cells allocated.
+//
+// Each parent thread gets allocated a destination region of n * 8 cells,
+// where n is the cell count in the parent thread.
+//
+// All space is allocated upfront in a large array, and written to in a
+// thread-safe manner. The Swift 'Array' data type is never used.
+
+// After a pass in the high levels, the source thread and source cell objects
+// must be deleted. In the low levels, the source thread doesn't perish
+// because we just directly recurse all remaining levels of the octree in
+// order.
+//
+// A function takes a single cell in, and returns an array of threads. If
+// the array has count one, it doesn't fork off the parent thread. The
+// overhead of array creation is relatively negligible at the high levels,
+// but vastly simplifies the implementation.
+//
+// The in-place atom buffer survives across passes / levels. It is
+// overwritten in-place by several threads, concurrently. While individual
+// threads may not access it contiguously, the memory operations should
+// theoretically be atomic / safe.
+//
+// A single task on the dispatch queue will be multiple function calls into
+// the tree traversal function. However, on first glance, the structure of
+// the calling loop might make it possible to fully inline. Don't worry
+// about ensuring it's fully inlined, as the overhead at high levels is low.
+
+// Instead of passing an offset'd pointer into each child function call, as
+// with previous algorithms (and likely the lower levels), we retain a
+// reference to the global base address. Instead, we identify the location
+// by a range. This range can recalculate the atom count too, simplifying the
+// amount of data passed between functions.
+//
+// 'Cell' owns the range.
+
+// ## Loop Over Levels of the Tree
+//
+// levelSize specified in this loop, which is the source of truth. No
+// division by 2 for child functions. This is a point of divergence with the
+// lower levels. The 'traverse' function should accept levelSize as an
+// argument.
+//
+// High-level data structure containing [Thread]:
+//
+// Start with a single Thread, and a single Cell scoped to the entire octree.
+// Invoke this thread with DispatchQueue.concurrentPerform, to simplify the
+// code. All passes now use concurrent dispatch.
+//
+// Inside each dispatch task, the number of cells is unpacked. Create a new
+// list of output cells. Sort them into ones belonging to the parent thread,
+// and a separate array of child Thread objects. Write the results to a
+// global buffer in a thread-safe way.
+//
+// In a post-processing part of the code, after the concurrent dispatch,
+// scan the global buffer of threads and cells. Eliminate any parent threads
+// that now have zero cells. Eliminate any allocated child threads that were
+// never materialized.
+//
+// Thread-safe data structure for results:
+//
+// Order of cells in a cell buffer could be inconsequential. A cell's range
+// always points to the same atoms, no matter where you store the cell in
+// memory. What's important is the Thread, which must reference the cell
+// objects.
+//
+// This process is especially tricky, because one thread could reference many
+// non-contiguous patches of atoms in the list. Does this mean many
+// non-contiguous patches of Cell objects? Not exactly.
+//
+// We can probably use a simple sequential scan-compact algorithm. Process
+// each thread in order from first to last. Update the thread based on some
+// running counters. Forget the current Thread object and move on to the next.
+
 extension OctreeSorter {
   struct Cell {
     var range: Range<Int>
     var origin: SIMD3<Float>
   }
   
-  // WARNING: This is not thread-safe. Need to prepare a different data
-  // organization scheme to make it thread-safe.
   struct Thread {
     var cells: [Cell]
   }
   
-  // Pseudocode of traversal algorithm:
-  //
-  // Each parent thread gets allocated n * 8 child threads, where n is the cell
-  // count in the parent thread. Each possible child up to 8 cells allocated.
-  //
-  // Each parent thread gets allocated a destination region of n * 8 cells,
-  // where n is the cell count in the parent thread.
-  //
-  // All space is allocated upfront in a large array, and written to in a
-  // thread-safe manner. The Swift 'Array' data type is never used.
+  struct WorkDistribution {
+    var inPlaceBuffer: UnsafeMutablePointer<UInt32>
+    var threads: [Thread]
+  }
   
-  // After a pass in the high levels, the source thread and source cell objects
-  // must be deleted. In the low levels, the source thread doesn't perish
-  // because we just directly recurse all remaining levels of the octree in
-  // order.
-  //
-  // A function takes a single cell in, and returns an array of threads. If
-  // the array has count one, it doesn't fork off the parent thread. The
-  // overhead of array creation is relatively negligible at the high levels,
-  // but vastly simplifies the implementation.
-  //
-  // The in-place atom buffer survives across passes / levels. It is
-  // overwritten in-place by several threads, concurrently. While individual
-  // threads may not access it contiguously, the memory operations should
-  // theoretically be atomic / safe.
-  //
-  // A single task on the dispatch queue will be multiple function calls into
-  // the tree traversal function. However, on first glance, the structure of
-  // the calling loop might make it possible to fully inline. Don't worry
-  // about ensuring it's fully inlined, as the overhead at high levels is low.
-  
-  // Instead of passing an offset'd pointer into each child function call, as
-  // with previous algorithms (and likely the lower levels), we retain a
-  // reference to the global base address. Instead, we identify the location
-  // by a range. This range can recalculate the atom count too, simplifying the
-  // amount of data passed between functions.
-  //
-  // 'Cell' owns the range.
-  
-  // ## Loop Over Levels of the Tree
-  //
-  // levelSize specified in this loop, which is the source of truth. No
-  // division by 2 for child functions. This is a point of divergence with the
-  // lower levels. The 'traverse' function should accept levelSize as an
-  // argument.
-  //
-  // High-level data structure containing [Thread]:
-  //
-  // Start with a single Thread, and a single Cell scoped to the entire octree.
-  // Invoke this thread with DispatchQueue.concurrentPerform, to simplify the
-  // code. All passes now use concurrent dispatch.
-  //
-  // Inside each dispatch task, the number of cells is unpacked. Create a new
-  // list of output cells. Sort them into ones belonging to the parent thread,
-  // and a separate array of child Thread objects. Write the results to a
-  // global buffer in a thread-safe way.
-  //
-  // In a post-processing part of the code, after the concurrent dispatch,
-  // scan the global buffer of threads and cells. Eliminate any parent threads
-  // that now have zero cells. Eliminate any allocated child threads that were
-  // never materialized.
-  //
-  // Thread-safe data structure for results:
-  //
-  // Order of cells in a cell buffer could be inconsequential. A cell's range
-  // always points to the same atoms, no matter where you store the cell in
-  // memory. What's important is the Thread, which must reference the cell
-  // objects.
-  //
-  // This process is especially tricky, because one thread could reference many
-  // non-contiguous patches of atoms in the list. Does this mean many
-  // non-contiguous patches of Cell objects? Not exactly.
-  //
-  // We can probably use a simple sequential scan-compact algorithm. Process
-  // each thread in order from first to last. Update the thread based on some
-  // running counters. Forget the current Thread object and move on to the next.
+  func mortonReorderingHighLevels() -> WorkDistribution {
+    fatalError("Not implemented.")
+  }
 }

24a226cda61018430c1b5bb5f844ed2f939a1a19
Philip Turner <philipturner.AR@gmail.com>
2025-09-08 15:35:27 -0400

    Finished drafting traverse for low levels

--- Sources/HDL/Topology/OctreeSorter/OctreeSorter+LowLevels.swift
+++ Sources/HDL/Topology/OctreeSorter/OctreeSorter+LowLevels.swift
@@ -48,5 +48,54 @@ extension OctreeSorter {
       return childNodeSizes
     }
     let childNodeSizes = createChildNodeSizes()
+    
+    // Transfer the scratch buffer to the input buffer.
+    func createChildNodeOffsets() -> SIMD8<UInt32> {
+      var childNodeOffsets: SIMD8<UInt32> = .zero
+      let scratchStart = UInt32(cellRange.startIndex * 8)
+      let scratchStride = UInt32(cellRange.count)
+      
+      var inPlaceOffset = cellRange.startIndex
+      for childNodeID in 0..<UInt32(8) {
+        let childNodeSize = childNodeSizes[Int(childNodeID)]
+        guard childNodeSize > 0 else {
+          continue
+        }
+        
+        let scratchOffset = scratchStart + childNodeID * scratchStride
+        (inPlaceBuffer + inPlaceOffset).initialize(
+          from: scratchBuffer + Int(scratchOffset),
+          count: Int(childNodeSize))
+        
+        childNodeOffsets[Int(childNodeID)] = UInt32(inPlaceOffset)
+        inPlaceOffset += Int(childNodeSize)
+      }
+      return childNodeOffsets
+    }
+    let childNodeOffsets = createChildNodeOffsets()
+    if (levelSize / 2) <= Float(1.0 / 32) {
+      return
+    }
+    
+    for childNodeID in 0..<UInt32(8) {
+      let childNodeSize = Int(childNodeSizes[Int(childNodeID)])
+      let inPlaceOffset = Int(childNodeOffsets[Int(childNodeID)])
+      if childNodeSize <= 1 {
+        continue
+      }
+      
+      func createNewOrigin() -> SIMD3<Float> {
+        let intOffset = (UInt8(childNodeID) &>> SIMD3(0, 1, 2)) & 1
+        let floatOffset = SIMD3<Float>(intOffset) * 2 - 1
+        return cellOrigin + floatOffset * levelSize / 4
+      }
+      
+      traverseLowLevel(
+        inPlaceBuffer: inPlaceBuffer,
+        scratchBuffer: scratchBuffer,
+        cellRange: inPlaceOffset..<(inPlaceOffset + childNodeSize),
+        cellOrigin: createNewOrigin(),
+        levelSize: levelSize / 2)
+    }
   }
 }

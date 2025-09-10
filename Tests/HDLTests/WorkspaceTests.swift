8e7c475f5273720755e9eaf3a62596b6d78288be
Philip Turner <philipturner.AR@gmail.com>
2025-09-10 09:15:51 -0400

    Fixed some bugs in the rendering

--- Tests/HDLTests/WorkspaceTests.swift
+++ Tests/HDLTests/WorkspaceTests.swift
@@ -7,33 +7,45 @@ final class WorkspaceTests: XCTestCase {
   func testWorkspace() throws {
     let atoms: [SIMD4<Float>] = [
       SIMD4(0.00, 0.00, 0.00, 6),
-      SIMD4(0.00, 0.00, 0.00, 6),
+      SIMD4(0.50, 0.00, 0.00, 6),
       SIMD4(0.00, 0.50, 0.00, 6),
       SIMD4(0.00, 0.00, 0.50, 6),
     ]
     
     // Specify the screen parameters.
-    let binSize: Float = 0.1
-    let binCount: Int = 10
+    let binSizes = SIMD2<Float>(0.05, 0.1)
+    let binCounts = SIMD2<Int>(20, 10)
     let origin = SIMD2<Float>(0.0, 0.0)
     var pixelArray = [Bool](
-      repeating: true,
-      count: binCount * binCount)
+      repeating: false,
+      count: binCounts.x * binCounts.y)
     
     // Mark the atoms on the screen.
     for atom in atoms {
       let coordsXY = SIMD2(atom.x, atom.y) - origin
-      let bin = (coordsXY / binSize).rounded(.down)
+      let bin = (coordsXY / binSizes).rounded(.down)
       let binInt = SIMD2<Int>(bin)
-      if any(binInt .< 0) || any(binInt .>= binCount) {
+      if any(binInt .< 0) || any(binInt .>= binCounts) {
         fatalError("Atom \(atom) was outside of bounds: \(bin)")
       }
       
-      let binMemoryIndex = binInt.y * binCount + binInt.x
+      let binMemoryIndex = binInt.y * binCounts.x + binInt.x
       pixelArray[binMemoryIndex] = true
     }
     
     // Display the screen.
-    
+    for pixelPositionY in (0..<binCounts.y).reversed() {
+      var line: String = ""
+      for pixelPositionX in 0..<binCounts.x {
+        let pixelMemoryIndex = pixelPositionY * binCounts.x + pixelPositionX
+        let pixelValue = pixelArray[pixelMemoryIndex]
+        if pixelValue {
+          line.append("X")
+        } else {
+          line.append(" ")
+        }
+      }
+      print(line)
+    }
   }
 }

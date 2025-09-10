13ecc7e6beb6a9a5cd90950b9edf468dd2cb30fb
Philip Turner <philipturner.AR@gmail.com>
2025-09-10 09:06:36 -0400

    Save progress

--- Tests/HDLTests/WorkspaceTests.swift
+++ Tests/HDLTests/WorkspaceTests.swift
@@ -7,11 +7,25 @@ final class WorkspaceTests: XCTestCase {
   func testWorkspace() throws {
     let atoms: [SIMD4<Float>] = [
       SIMD4(0.00, 0.00, 0.00, 6),
-      SIMD4(0.50, 0.00, 0.00, 6),
+      SIMD4(0.00, 0.00, 0.00, 6),
       SIMD4(0.00, 0.50, 0.00, 6),
       SIMD4(0.00, 0.00, 0.50, 6),
     ]
     
     // Objective: render these atoms in a 2D command-line render.
+    let binSize: Float = 0.1
+    let binCount: Int = 10
+    var pixelArray = [Bool](
+      repeating: true,
+      count: binCount * binCount)
+    
+    for atom in atoms {
+      let coordsXY = SIMD2(atom.x, atom.y)
+      let bin = (coordsXY / binSize).rounded(.down)
+      let binInt = SIMD2<Int>(bin)
+      if any(binInt .< 0) || any(binInt .>= binCount) {
+        fatalError("Atom \(atom) was outside of bounds: \(bin)")
+      }
+    }
   }
 }

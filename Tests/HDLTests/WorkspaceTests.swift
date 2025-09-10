2bd66563b4cfd7bd9789a8ab188a9db7b0dc03f6
Philip Turner <philipturner.AR@gmail.com>
2025-09-10 08:54:14 -0400

    Started the hello world test

---
+++ Tests/HDLTests/WorkspaceTests.swift
@@ -0,0 +1,10 @@
+import HDL
+import XCTest
+
+// Temporary file for treating the Swift tests like a scratch workspace.
+
+final class WorkspaceTests: XCTestCase {
+  func testWorkspace() throws {
+    print("Hello, world.")
+  }
+}

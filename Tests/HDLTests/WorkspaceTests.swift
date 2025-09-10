a6e5765f06ada6bd303e46abf18b9837f40a6950
Philip Turner <philipturner.AR@gmail.com>
2025-09-10 16:42:32 -0400

    Generating the correct family of structure

--- Tests/HDLTests/WorkspaceTests.swift
+++ Tests/HDLTests/WorkspaceTests.swift
@@ -5,53 +5,49 @@ import XCTest
 
 final class WorkspaceTests: XCTestCase {
   func testWorkspace() throws {
-    #if false
-    let lattice = Lattice<Hexagonal> { h, k, l in
-      let h2k = h + 2 * k
-      Bounds { 0 * h + 2 * h2k + 3 * l }
-      Material { .elemental(.carbon) }
-    }
-    
-//    let lattice = Lattice<Cubic> { h, k, l in
-//      Bounds { 0 * h + 2 * k + 0 * l }
-//      Material { .elemental(.carbon) }
-//    }
-    
-    // Specify the screen parameters.
-    let binSizes = SIMD2<Float>(0.025, 0.05)
-    let binCounts = SIMD2<Int>(80, 40)
-    let origin = SIMD2<Float>(0.0, 0.0)
-    var pixelArray = [Bool](
-      repeating: false,
-      count: binCounts.x * binCounts.y)
-    
-    // Mark the atoms on the screen.
-    for atom in lattice.atoms {
-      let coordsXY = SIMD2(atom.x, atom.y) - origin
-      let bin = (coordsXY / binSizes).rounded(.down)
-      let binInt = SIMD2<Int>(bin)
-      if any(binInt .< 0) || any(binInt .>= binCounts) {
-        fatalError("Atom \(atom) was outside of bounds: \(bin)")
-      }
+    let lattice = Lattice<Cubic> { h, k, l in
+      Bounds { 20 * (h + k + l) }
+      Material { .checkerboard(.silicon, .carbon) }
       
-      let binMemoryIndex = binInt.y * binCounts.x + binInt.x
-      pixelArray[binMemoryIndex] = true
-    }
-    
-    // Display the screen.
-    for pixelPositionY in (0..<binCounts.y).reversed() {
-      var line: String = ""
-      for pixelPositionX in 0..<binCounts.x {
-        let pixelMemoryIndex = pixelPositionY * binCounts.x + pixelPositionX
-        let pixelValue = pixelArray[pixelMemoryIndex]
-        if pixelValue {
-          line.append("X")
-        } else {
-          line.append(" ")
+      let beamWidth: Float = 2
+      
+      Volume {
+        Concave {
+          Convex {
+            Origin { beamWidth * h }
+            Plane { h }
+          }
+          Convex {
+            Origin { beamWidth * k }
+            Plane { k }
+          }
         }
+        
+        Concave {
+          Convex {
+            Origin { beamWidth * h }
+            Plane { h }
+          }
+          Convex {
+            Origin { beamWidth * l }
+            Plane { l }
+          }
+        }
+        
+        Concave {
+          Convex {
+            Origin { beamWidth * k }
+            Plane { k }
+          }
+          Convex {
+            Origin { beamWidth * l }
+            Plane { l }
+          }
+        }
+        
+        Replace { .empty }
       }
-      print(line)
     }
-    #endif
+    print(lattice.atoms.count)
   }
 }

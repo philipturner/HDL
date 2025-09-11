7dc06e023818539165879345f3c95a66c220ebce
Philip Turner <philipturner.AR@gmail.com>
2025-09-11 08:50:09 -0400

    Confirmed the suitability of QueryPerformanceCounter

--- Tests/HDLTests/Resources/Profiler.swift
+++ Tests/HDLTests/Resources/Profiler.swift
@@ -4,13 +4,31 @@ import Darwin
 import WinSDK
 #endif
 
+// Suitability of QueryPerformanceCounter for FP64 numbers:
+
+/*
+import WinSDK
+
+var frequency = LARGE_INTEGER()
+let boolean = QueryPerformanceCounter(&frequency)
+
+print(frequency.LowPart as UInt32)
+print(frequency.HighPart as Int32)
+print(frequency.QuadPart as Int64)
+print(1 << 53)
+
+// 2085122834
+// 17871
+// 76757445669650
+// 9007199254740992
+*/
+
 struct Profiler {
   static func time() -> Double {
     func queryTickCount() -> UInt64 {
       #if os(macOS)
       return mach_continuous_time()
       #else
-      // Hope this is robust on Windows...
       var largeInteger = LARGE_INTEGER()
       QueryPerformanceCounter(&largeInteger)
       return UInt64(largeInteger.QuadPart)
